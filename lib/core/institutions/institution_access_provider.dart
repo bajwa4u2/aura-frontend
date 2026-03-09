e13fa3b83d9f49a5466a03080f36e2fc67285fd5
bajwa4u2 <muhammadsakhawat@gmail.com>
2026-03-08 23:09:50 -0400

    Remove enter institution screen and gate institution access behind session bootstrap

--- lib/core/institutions/institution_access_provider.dart
+++ lib/core/institutions/institution_access_provider.dart
@@ -1,6 +1,8 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../auth/auth_providers.dart';
+import '../auth/session_bootstrap.dart';
 import '../net/dio_provider.dart';
 
 enum InstitutionAccessState {
@@ -29,45 +31,60 @@ class InstitutionAccess {
       state == InstitutionAccessState.authorizedSpeaker;
 }
 
-final institutionAccessProvider =
-    FutureProvider<InstitutionAccess>((ref) async {
+final institutionAccessProvider = FutureProvider<InstitutionAccess>((ref) async {
+  await ref.watch(sessionBootstrapProvider.future);
+
+  final authStatus = ref.watch(authStatusProvider);
+  if (authStatus != AuthStatus.authed) {
+    return const InstitutionAccess(
+      state: InstitutionAccessState.none,
+    );
+  }
+
   final dio = ref.watch(dioProvider);
 
   try {
     final res = await dio.get('/institutions/me');
 
     final data = Map<String, dynamic>.from(res.data);
-
-    final state = (data['state'] ?? '').toString();
+    final state = (data['state'] ?? '').toString().trim();
 
     InstitutionAccessState parsed;
-
     switch (state) {
       case 'PENDING_REQUEST':
         parsed = InstitutionAccessState.pending;
         break;
-
       case 'VERIFIED_MEMBER':
         parsed = InstitutionAccessState.verifiedMember;
         break;
-
       case 'AUTHORIZED_SPEAKER':
         parsed = InstitutionAccessState.authorizedSpeaker;
         break;
-
       default:
         parsed = InstitutionAccessState.none;
     }
 
     return InstitutionAccess(
       state: parsed,
-      institution: data['institution'],
-      membership: data['membership'],
-      request: data['request'],
+      institution: data['institution'] is Map
+          ? Map<String, dynamic>.from(data['institution'])
+          : null,
+      membership: data['membership'] is Map
+          ? Map<String, dynamic>.from(data['membership'])
+          : null,
+      request: data['request'] is Map
+          ? Map<String, dynamic>.from(data['request'])
+          : null,
     );
-  } on DioException catch (_) {
-    return const InstitutionAccess(
-      state: InstitutionAccessState.none,
-    );
+  } on DioException catch (e) {
+    final code = e.response?.statusCode;
+
+    if (code == 401 || code == 403) {
+      return const InstitutionAccess(
+        state: InstitutionAccessState.none,
+      );
+    }
+
+    rethrow;
   }
 });
